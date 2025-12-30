f71cc700235a41ba54bf330aeeb2a658e571a30e
Carou <carolinecoelho.a@gmail.com>
2025-12-29 21:06:49 -0300

    Alterações adicionais curso

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
 document.getElementById("botaoEnviar").addEventListener("click",validaFormulario)
 
-function validaFormulario(){
-    if(document.getElementById("nome").value != "" && document.getElementById("email").value != "" &&document.getElementById("telefone").value != "" ){
-    alert("Prontinho! Você receberá as novidades por email.")
-    }else{
-    alert("Por favor, preencha os campos nome, email e telefone!")
+
+function validaFormulario() {
+    let nome = document.getElementById("nome").value;
+    let email = document.getElementById("email").value;
+    let telefone = document.getElementById("telefone").value;
+
+    if (nome != "" && email != "" && telefone != "") {
+        alert("Prontinho! Você receberá as novidades por email.");
+    } else {
+        alert("Por favor, preencha os campos nome, email e telefone!");
     }
 }
 
